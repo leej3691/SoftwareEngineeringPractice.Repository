35940e2b4553081c00912a790f82e7025cdf80db
Lee Jones <leejones3691@icloud.com>
2020-02-17 17:59:21 +0000

    Updated identity model

--- EstateAgents.Library/EstateAgents.Library/DAL/EstateAgentsDbContext.cs
+++ EstateAgents.Library/EstateAgents.Library/DAL/EstateAgentsDbContext.cs
@@ -1,34 +1,14 @@
-﻿using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using System.Data.Entity;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 
 namespace EstateAgents.Library.DAL
 {
-    public class ApplicationUser : IdentityUser
-    {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
-    }
-
-    public class EstateAgentsDbContext : IdentityDbContext<ApplicationUser>
+    public class EstateAgentsDbContext : DbContext
     {
 
         public EstateAgentsDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DefaultConnection")
         {
            
-        }
-
-        public static EstateAgentsDbContext Create()
-        {
-            return new EstateAgentsDbContext();
         }
 
         //public virtual DbSet<TestTable> TestTable { get; set; }
