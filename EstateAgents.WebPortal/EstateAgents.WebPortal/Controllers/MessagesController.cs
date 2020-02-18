a517e86177cb6cdb9e40d6efd63f759685ca4143
Lee Jones <leejones3691@icloud.com>
2020-02-18 21:31:42 +0000

    Create blank views

---
+++ EstateAgents.WebPortal/EstateAgents.WebPortal/Controllers/MessagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EstateAgents.WebPortal.Controllers
+{
+    public class MessagesController : Controller
+    {
+        // GET: Messages
+        public ActionResult Messages()
+        {
+            return View();
+        }
+    }
+}
---
+++ EstateAgents.WebPortal/EstateAgents.WebPortal/Controllers/PropertyController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EstateAgents.WebPortal.Controllers
+{
+    public class PropertyController : Controller
+    {
+        // GET: Property
+        public ActionResult PropertyShowroom()
+        {
+            return View();
+        }
+
+        public ActionResult PropertySaved()
+        {
+            return View();
+        }
+
+        public ActionResult PropertyMakeOffer()
+        {
+            return View();
+        }
+
+        public ActionResult PropertyDetails()
+        {
+            return View();
+        }
+
+        public ActionResult PropertyBookViewing()
+        {
+            return View();
+        }
+
+    }
+}
