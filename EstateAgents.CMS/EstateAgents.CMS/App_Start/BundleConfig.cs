b09b0a4de03829a50864906419056ccbb304cc3b
Lee Jones <leejones3691@icloud.com>
2020-03-08 14:45:34 +0000

    CMS setup

--- EstateAgents.CMS/EstateAgents.CMS/App_Start/BundleConfig.cs
+++ EstateAgents.CMS/EstateAgents.CMS/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using BundleTransformer.Core.Bundles;
+using System.Web;
 using System.Web.Optimization;
 
 namespace EstateAgents.CMS
@@ -8,6 +9,11 @@ namespace EstateAgents.CMS
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.UseCdn = true;
+            bundles.Add(new CustomStyleBundle("~/style/bootstrap").Include("~/content/bootswatch/build.less"));
+            bundles.Add(new StyleBundle("~/style/jquery-datatables-bootstrap", "https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css"));
+            bundles.Add(new StyleBundle("~/style/jquery-datatables-buttons", "https://cdn.datatables.net/buttons/1.10.12/css/buttons.bootstrap.min.css"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -25,6 +31,14 @@ namespace EstateAgents.CMS
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/style/font-awesome", "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css").Include("~/content/font-awesome.css"));
+            bundles.Add(new ScriptBundle("~/scripts/jquery-datatables", "https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"));
+            bundles.Add(new ScriptBundle("~/scripts/jquery-datatables-buttons", "https://cdn.datatables.net/buttons/1.10.12/js/dataTables.buttons.min.js"));
+            bundles.Add(new ScriptBundle("~/scripts/jquery-datatables-buttons-bootstrap", "https://cdn.datatables.net/buttons/1.10.12/js/buttons.bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/scripts/jquery-datatables-colvis", "https://cdn.datatables.net/buttons/1.10.12/js/buttons.colVis.min.js"));
+            bundles.Add(new ScriptBundle("~/scripts/jquery-datatables-buttons-html5", "https://cdn.datatables.net/buttons/1.10.12/js/buttons.html5.min.js"));
+            bundles.Add(new ScriptBundle("~/scripts/jquery-datatables-bootstrap", "https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"));
         }
     }
 }
---
+++ EstateAgents.CMS/EstateAgents.CMS/Models/sitemap.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace EstateAgents.CMS.Models
+{
+    public class SiteMap
+    {
+        public XElement Root { get; set; }
+        public List<XElement> Nodes { get; set; }
+
+        public SiteMap()
+        {
+
+            string siteMap = "~/Sitemaps/default.sitemap";
+
+            if (HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                siteMap = "~/Sitemaps/employee.sitemap";
+            }
+
+            this.Root = XElement.Load(HttpContext.Current.Server.MapPath(siteMap));
+
+            this.Nodes = Root.Elements().ToList();
+        }
+    }
+}
